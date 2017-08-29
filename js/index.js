adf72f793ca8b5f87c8702d65ce534c006e2f953
Nataliya <https.ka@gmail.com>
2017-08-29 19:07:31 +0000

    without checkbox

--- js/index.js
+++ js/index.js
@@ -1,26 +1,23 @@
-$(function(){
-  $('input[type=submit]').click(send);
-})
+$(document).ready(function() {
+    
+    $("#form").submit(function(){
+        $( "input[type=radio][name=gender]:checked" ).val();
+        $( "select#city option:checked" ).val();
+        $( "select#moviehouse option:checked" ).val();
+        $( "select#rooms option:checked").val();
+        $( "select#genre option:checked").val();
+        $( "input[type=checkbox][name=movie]:checked").val();
+        $( "input[type=radio][name=question]:checked" ).val();
 
-function send(e) {
-  e.preventDefault();
-  
-  if (!$('form')[0].checkValidity()) {
-    $('#incorrect').show();
-    return;
-  }
-  $.ajax({
-    url: "https://formspree.io/https.ka@gmail.com", 
-    method: "POST",
-    data: {
-      name: $('#name').val(),
-      surname: $('#surname').val(),
-      age: $('#age').val(),
-      email: $('#email').val()
-    },
-    dataType: "json"
-  }).done(function(){
-    $('form')[0].reset();
-    $('#thanks').show();
-  })
-}
+       $.ajax ({
+            url: "https://formspree.io/https.ka@gmail.com",
+            method: "POST",
+            dataType: "json",
+            data:$(this).serialize()
+       }).done(function(){
+            $('form')[0].reset();
+            $('#thanks').show();
+       })
+       return false;
+    });
+});
