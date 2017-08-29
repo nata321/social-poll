5907fcd102acc6d3440ac9b7942caa173f7aaa52
Nataliya <https.ka@gmail.com>
2017-08-29 19:27:28 +0000

    and

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 $(document).ready(function() {
     
     $("#form").submit(function(){
-        $( "input[type=radio][name=gender]:checked" ).val();
-        $( "select#city option:checked" ).val();
-        $( "select#moviehouse option:checked" ).val();
-        $( "select#rooms option:checked").val();
-        $( "select#genre option:checked").val();
-        $( "input[type=checkbox][name=movie]:checked").val();
-        $( "input[type=radio][name=question]:checked" ).val();
-
+       
        $.ajax ({
             url: "https://formspree.io/https.ka@gmail.com",
             method: "POST",
