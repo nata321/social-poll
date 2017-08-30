0873de34e74e026374c6bb1495d59e82dd2ad503
Nataliya <https.ka@gmail.com>
2017-08-30 10:29:09 +0000

    s

--- js/index.js
+++ js/index.js
@@ -18,14 +18,6 @@ $(function(){
 })
 function sendData(e) {
     e.preventDefault();
-    let movieOne = $('#movie-1').val();
-    let movieTwo = $('#movie-2').val();
-    let movieThree = $('#movie-3').val();
-    let movieFour = $('#movie-4').val();
-    let animationOne = $('#animation-1').val();
-    let animationTwo = $('#animation-2').val();
-    let animationThree = $('#animation-3').val();
-    let animationFour = $('#animation-4').val();
     if (!$('form')[0].checkValidity()) {
     $('#incorrect').show();
     return;
