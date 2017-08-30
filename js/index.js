658c4ac5f5d6cb965b28bad790b5359a004ce3ee
Nataliya <https.ka@gmail.com>
2017-08-30 11:22:11 +0000

    s

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-// $(document).ready(function() {
-
-// $("#form").submit(function(){
-//   $.ajax ({
-//         url: "https://formspree.io/https.ka@gmail.com",
-//         method: "POST",
-//         dataType: "json",
-//         data:$(this).serialize()
-//   }).done(function(){
-//         $('form')[0].reset();
-//         $('#thanks').show();
-//   })
-//     return false;
-//     });
-// });
 $(function(){
   $('input[type=submit').click(sendData);
 });
