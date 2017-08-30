c2c0ca24447203bdb044c0a18f3282062dfca201
Nataliya <https.ka@gmail.com>
2017-08-30 10:17:58 +0000

    ff

--- js/index.js
+++ js/index.js
@@ -1,29 +1,44 @@
-$(function(){
-  $('input[type=submit').click(sendData);
-})
-function sendData(e) {
-    e.preventDefault();
-    let movieOne = $('#movie-1').val();
-    let movieTwo = $('#movie-2').val();
-    let movieThree = $('#movie-3').val();
-    let movieFour = $('#movie-4').val();
-    let animationOne = $('#animation-1').val();
-    let animationTwo = $('#animation-2').val();
-    let animationThree = $('#animation-3').val();
-    let animationFour = $('#animation-4').val();
-    if (!$('form')[0].checkValidity()) {
-    $('#incorrect').show();
-    return;
-  }
-  $.ajax({
-        url: "https://formspree.io/https.ka@gmail.com", 
+ $(document).ready(function() {
+
+$("#form").submit(function(){
+  $.ajax ({
+        url: "https://formspree.io/https.ka@gmail.com",
         method: "POST",
         dataType: "json",
-        data: {
-            data: {inputs: $('form').serialize()}
-        }
-      }).done(function(){
+        data:$(this).serialize()
+  }).done(function(){
         $('form')[0].reset();
         $('#thanks').show();
-      })
-    }
+  })
+  return false;
+});
+});
+// $(function(){
+//   $('input[type=submit').click(sendData);
+// })
+// function sendData(e) {
+//     e.preventDefault();
+//     let movieOne = $('#movie-1').val();
+//     let movieTwo = $('#movie-2').val();
+//     let movieThree = $('#movie-3').val();
+//     let movieFour = $('#movie-4').val();
+//     let animationOne = $('#animation-1').val();
+//     let animationTwo = $('#animation-2').val();
+//     let animationThree = $('#animation-3').val();
+//     let animationFour = $('#animation-4').val();
+//     if (!$('form')[0].checkValidity()) {
+//     $('#incorrect').show();
+//     return;
+//   }
+//   $.ajax({
+//         url: "https://formspree.io/https.ka@gmail.com", 
+//         method: "POST",
+//         dataType: "json",
+//         data: {
+//             data: {inputs: $('form').serialize()}
+//         }
+//       }).done(function(){
+//         $('form')[0].reset();
+//         $('#thanks').show();
+//       })
+//     }
