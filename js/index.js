76e0355688d2190c5614430e809446ee2e6dce66
Nataliya <https.ka@gmail.com>
2017-08-30 10:09:32 +0000

    end

--- js/index.js
+++ js/index.js
@@ -1,48 +1,21 @@
-$(document).ready(function() {
-    
-    $("#form").submit(function(){
-      $.ajax ({
-            url: "https://formspree.io/https.ka@gmail.com",
-            method: "POST",
-            dataType: "json",
-            data:$(this).serialize()
+$(function(){
+  $('input[type=submit').click(sendData);
+})
+function sendData(e) {
+    e.preventDefault();
+    if (!$('form')[0].checkValidity()) {
+    $('#incorrect').show();
+    return;
+  }
+  $.ajax({
+        url: "https://formspree.io/https.ka@gmail.com", 
+        method: "POST",
+        dataType: "json",
+        data: {
+            data: {inputs: $('form').serialize()}
+        }
       }).done(function(){
-            $('form')[0].reset();
-            $('#thanks').show();
+        $('form')[0].reset();
+        $('#thanks').show();
       })
-      return false;
-    });
-});
-// $(function(){
-//   $('input[type=submit').click(sendData);
-// })
-// function sendData(e) {
-//   e.preventDefault();
-//   if (!$('form')[0].checkValidity()) {
-//     $('#incorrect').show();
-//     return;
-//   }
-//   $.ajax({
-//         url: "https://formspree.io/https.ka@gmail.com", 
-//         method: "POST",
-//         dataType: "json",
-//         data: {
-//             name: $('#name').val(),
-//             surname: $('#surname').val(),
-//             age: $('#age').val(),
-//             email: $('#email').val(),
-//             gender: $( "[type=radio][name=gender]:checked" ).val(),
-//             city: $( "select#city option:checked" ).val(),
-//             moviehouse: $( "select#moviehouse option:checked" ).val(),
-//             rooms: $( "select#rooms option:checked").val(),
-//             genre: $( "select#genre option:checked").val(),
-//             movie: $( "input[type=checkbox][name=movie]:checked").val(),
-//             animation: $( "input[type=checkbox][name=animation]:checked").val(),
-//             question: $( "input[type=radio][name=question]:checked" ).val(),
-//             opinion: $('#opinion').val()
-//     }
-//   }).done(function(){
-//     $('form')[0].reset();
-//     $('#thanks').show();
-//   })
-// }
+    }
